918c1b35f4f67e0c30f72dcd28a501b3f32a6741
Jonathan Oliveira <devjoliveira@gmail.com>
2026-06-25 19:42:20 -0300

    Add a base service

---
+++ frontend/src/app/core/service/base.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+export abstract class BaseService<Request, Response> {
+
+  protected http = inject(HttpClient);
+
+  protected abstract endpoint: string;
+
+  private get url(): string {
+    return `${environment.apiUrl}/${this.endpoint}`;
+  }
+
+  getAll() {
+    return this.http.get<Response[]>(this.url);
+  }
+
+  getById(id: number | string) {
+    return this.http.get<Response>(`${this.url}/${id}`);
+  }
+
+  create(data: Request) {
+    return this.http.post<Response>(this.url, data);
+  }
+
+  update(id: number | string, data: Request) {
+    return this.http.put<Response>(`${this.url}/${id}`, data);
+  }
+
+  delete(id: number | string) {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
