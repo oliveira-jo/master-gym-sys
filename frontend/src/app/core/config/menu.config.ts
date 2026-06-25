4fe2db6ac1bc57b64ee7a5d9171fb4cf3d47a048
Jonathan Oliveira <devjoliveira@gmail.com>
2026-06-25 17:20:18 -0300

    Create menu config

---
+++ frontend/src/app/core/config/menu.config.ts
@@ -0,0 +1,38 @@
+import { MenuItem } from '../model/menu-item';
+
+export const MENU_CONFIG: MenuItem[] = [
+
+  {
+    label: 'Sistemas',
+    children: [
+      { label: 'Usuários', route: '/usuarios' },
+      { label: 'Sair' }
+    ]
+  },
+
+  {
+    label: 'Cadastros',
+    children: [
+      { label: 'Alunos', route: '/alunos' },
+      { label: 'Modalidades', route: '/modalidades' },
+      { label: 'Planos', route: '/planos' }
+    ]
+  },
+
+  {
+    label: 'Processos',
+    children: [
+      { label: 'Matricular', route: '/matriculas' },
+      { label: 'Faturas', route: '/faturas' },
+      { label: 'Pagamentos', route: '/pagamentos' }
+    ]
+  },
+
+  {
+    label: 'Relatórios',
+    children: [
+      { label: 'Matrículas', route: '/relatorios' }
+    ]
+  }
+
+];
--- frontend/src/app/layout/navbar/navbar.component.ts
+++ frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MenuItem } from '../../core/models/menu-item';
 
 @Component({
   selector: 'app-navbar',
@@ -8,4 +9,70 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  menus: MenuItem[] = [
+
+    {
+      label: 'Sistemas',
+      children: [
+        {
+          label: 'Usuários',
+          route: '/usuarios'
+        },
+        {
+          label: 'Sair'
+        }
+      ]
+    },
+
+    {
+      label: 'Cadastros',
+      children: [
+        {
+          label: 'Alunos',
+          route: '/alunos'
+        },
+        {
+          label: 'Modalidades',
+          route: '/modalidades'
+        },
+        {
+          label: 'Planos',
+          route: '/planos'
+        }
+      ]
+    },
+
+    {
+      label: 'Processos',
+      children: [
+        {
+          label: 'Matricular',
+          route: '/matriculas'
+        },
+        {
+          label: 'Gerar Faturas',
+          route: '/faturas'
+        },
+        {
+          label: 'Pagamentos',
+          route: '/pagamentos'
+        }
+      ]
+    },
+
+    {
+      label: 'Relatórios',
+      children: [
+        {
+          label: 'Matrículas',
+          route: '/relatorios'
+        }
+      ]
+    }
+
+  ];
+}
+
+
