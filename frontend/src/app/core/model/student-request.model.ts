944ed3b1239dab387069a52eb83b80991e491e8a
Jonathan Oliveira <devjoliveira@gmail.com>
2026-06-28 19:10:28 -0300

    Front: create modal for student

--- frontend/src/app/core/model/student-request.model.ts
+++ frontend/src/app/core/model/student-request.model.ts
@@ -1,15 +1,17 @@
-export interface StudentRequest {
-  name: string;
-  birthdate: string; // dd/MM/yyyy
-  genre: string; // 1 char
-  phone: string;
-  email: string;
-  cpf: string;
-  observations?: string;
-  address: string;
-  number: string;
-  complement?: string;
-  city: string;
-  state: string;
-  zipCode: string;
+export class StudentRequest {
+
+  name = '';
+  birthdate = '';
+  genre = '';// 1 char
+  phone = '';
+  email = '';
+  cpf = '';
+  address = '';
+  number = '';
+  complement = '';
+  city = '';
+  state = '';
+  zipCode = '';
+  observations = '';
+
 }
