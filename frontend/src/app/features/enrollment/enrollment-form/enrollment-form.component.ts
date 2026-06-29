5a31241deb4430ed2f1be8276c410308551cabd7
Jonathan Oliveira <devjoliveira@gmail.com>
2026-06-29 19:09:33 -0300

    Front: fix modalities in form and list

--- frontend/src/app/features/enrollment/enrollment-form/enrollment-form.component.ts
+++ frontend/src/app/features/enrollment/enrollment-form/enrollment-form.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Inject, Input, Output } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EnrollmentResponse } from '../../../core/model/enrollment-response.model';
+import { EnrollmentService } from '../../../core/service/enrollment.service';
+import { EnrollmentRequest } from '../../../core/model/enrollment-request.model';
 
 @Component({
   selector: 'app-enrollment-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './enrollment-form.component.html',
   styleUrl: './enrollment-form.component.css'
 })
 export class EnrollmentFormComponent {
 
+  @Input() enrollment?: EnrollmentResponse;
+  @Output() close = new EventEmitter<boolean>();
+
+  private fb = Inject(NonNullableFormBuilder);
+  private enrollmentService = inject(EnrollmentService);
+
+  form = this.fb.group({
+    enrollmentDate: ['', Validators.required],
+    dueDay: ['', Validators.required],
+    closingDate: ['', Validators.required],
+    studentId: ['', Validators.required],
+  });
+
+  ngOnInit() {
+    if (this.enrollment) {
+      this.form.patchValue({
+        ...this.enrollment,
+      });
+    }
+  }
+
+  save(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const request: EnrollmentRequest = this.form.getRawValue();
+
+
+    if (this.enrollment) {
+      this.enrollmentService
+        .update(this.enrollment.id, request)
+        .subscribe(() => this.close.emit(true));
+
+    } else {
+      this.enrollmentService
+        .create(request)
+        .subscribe(() => this.close.emit(true));
+
+    }
+
+  }
+
+  ngOnChanges(): void {
+    if (!this.enrollment) {
+      this.form.reset();
+      return;
+    }
+
+    this.form.patchValue({
+      enrollmentDate: this.enrollment.enrollmentDate,
+      dueDay: this.enrollment.dueDay,
+      closingDate: this.enrollment.closingDate,
+      studentId: this.enrollment.studentId,
+    });
+  }
+
+  cancel() {
+    this.close.emit(false);
+  }
 }
--- frontend/src/app/features/enrollment/enrollment-list/enrollment-list.component.ts
+++ frontend/src/app/features/enrollment/enrollment-list/enrollment-list.component.ts
@@ -1,11 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { EnrollmentFormComponent } from '../enrollment-form/enrollment-form.component';
+import { EnrollmentService } from '../../../core/service/enrollment.service';
+import { EnrollmentResponse } from '../../../core/model/enrollment-response.model';
+import { PageResponse } from '../../../core/model/page/page-response.model';
+import { PageableRequest } from '../../../core/model/page/pageable-request.model';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-enrollment-list',
-  imports: [],
+  imports: [EnrollmentFormComponent],
   templateUrl: './enrollment-list.component.html',
   styleUrl: './enrollment-list.component.css'
 })
-export class EnrollmentListComponent {
+export class EnrollmentListComponent implements OnInit {
+
+  private readonly enrollmentService = inject(EnrollmentService);
+
+  enrollments: EnrollmentResponse[] = [];
+
+  pageResponse?: PageResponse<EnrollmentResponse>;
+
+  page = new PageableRequest(0, 10, 'name');
+
+  totalElements = 0;
+  selectedEnrollment?: EnrollmentResponse;
+  modalOpen = false;
+
+  // 
+  private fb = inject(FormBuilder);
+  searchForm = this.fb.group({
+    enrollmentDate: [''],
+    dueDay: [''],
+    closingDate: [''],
+    studentId: [''],
+  });
+
+
+  ngOnInit(): void {
+    this.loadEnrollments();
+  }
+
+  loadEnrollments(): void {
+
+    this.enrollmentService
+      .findAll(this.page)
+      .subscribe({
+
+        next: page => {
+          this.enrollments = page.content;
+          this.totalElements = page.totalElements;
+        },
+
+        error: err => console.error(err)
+
+      });
+  }
+
+  search(): void {
+    this.page.page = 0;
+    this.loadEnrollments();
+  }
+
+  clear(): void {
+    this.searchForm.reset();
+    this.page.page = 0;
+    this.loadEnrollments();
+  }
+
+  openCreate(): void {
+    this.selectedEnrollment = undefined;
+    this.modalOpen = true;
+  }
+
+  openEdit(enrollment: EnrollmentResponse): void {
+    this.selectedEnrollment = enrollment;
+    this.modalOpen = true;
+  }
+
+  delete(id: number): void {
+
+    if (!confirm('Deseja realmente excluir este inscrição?')) {
+      return;
+    }
+    this.enrollmentService.delete(id).subscribe({
+      next: () => {
+        this.loadEnrollments();
+      },
+      error: err => console.error(err)
+
+    });
+
+  }
+
+  closeModal(refresh: boolean): void {
+    this.modalOpen = false;
+    if (refresh) {
+      this.loadEnrollments();
+    }
+  }
 
 }
