5c4c37eecf3a016b52fc4fd487b55379ba93b7e0
Jonathan Oliveira <devjoliveira@gmail.com>
2026-06-29 17:47:47 -0300

    Front: fis lists and forms

--- frontend/src/app/features/modality/modality-list/modality-list.component.ts
+++ frontend/src/app/features/modality/modality-list/modality-list.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ModalityService } from '../../../core/service/modality.service';
+import { ModalityResponse } from '../../../core/model/modality-response.model';
+import { PageResponse } from '../../../core/model/page/page-response.model';
+import { PageableRequest } from '../../../core/model/page/pageable-request.model';
+import { FormBuilder } from '@angular/forms';
+import { ModalityRequest } from '../../../core/model/modality-request.model';
 
 @Component({
   selector: 'app-modality-list',
@@ -6,6 +12,93 @@ import { Component } from '@angular/core';
   templateUrl: './modality-list.component.html',
   styleUrl: './modality-list.component.css'
 })
-export class ModalityListComponent {
+export class ModalityListComponent implements OnInit {
+
+  private readonly modalityService = inject(ModalityService);
+
+  students: ModalityResponse[] = [];
+
+  pageResponse?: PageResponse<ModalityResponse>;
+
+  page = new PageableRequest(0, 10, 'name');
+
+  totalElements = 0;
+  selectedModality?: ModalityResponse;
+  modalOpen = false;
+
+  // 
+  private fb = inject(FormBuilder);
+  searchForm = this.fb.group({
+    name: ['']
+  });
+
+
+  ngOnInit(): void {
+    this.loadModalities();
+  }
+
+  loadModalities(): void {
+    const filter: ModalityRequest = {
+      name: this.searchForm.value.name ?? ''
+    };
+
+    this.modalityService
+      .findAll(this.page)
+      .subscribe({
+
+        next: page => {
+          this.students = page.content;
+          this.totalElements = page.totalElements;
+        },
+
+        error: err => console.error(err)
+
+      });
+  }
+
+  search(): void {
+
+    this.page.page = 0;
+    this.loadModalities();
+
+  }
+
+  clear(): void {
+    this.searchForm.reset();
+    this.page.page = 0;
+    this.loadModalities();
+  }
+
+  openCreate(): void {
+    this.selectedModality = undefined;
+    this.modalOpen = true;
+  }
+
+  openEdit(modality: ModalityResponse): void {
+    this.selectedModality = modality;
+    this.modalOpen = true;
+  }
+
+  delete(id: number): void {
+
+    if (!confirm('Deseja realmente excluir este modalidade?')) {
+      return;
+    }
+    this.modalityService.delete(id).subscribe({
+      next: () => {
+        this.loadModalities();
+      },
+      error: err => console.error(err)
+
+    });
+
+  }
+
+  closeModal(refresh: boolean): void {
+    this.modalOpen = false;
+    if (refresh) {
+      this.loadModalities();
+    }
+  }
 
 }
