0175b3100be1eafb831470c84e49f38fb4103c69
Jonathan Oliveira <devjoliveira@gmail.com>
2026-06-29 22:01:14 -0300

    Front: create list and form of subscription

--- frontend/src/app/features/subscription/subscription-form/subscription-form.component.ts
+++ frontend/src/app/features/subscription/subscription-form/subscription-form.component.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SubscriptionResponse } from '../../../core/model/subscription-response.model';
+import { SubscriptionService } from '../../../core/service/subscription.service';
+import { SubscriptionRequest } from '../../../core/model/subscription-request.model';
+import { ModalityResponse } from '../../../core/model/modality-response.model';
+import { ModalityService } from '../../../core/service/modality.service';
+import { PageableRequest } from '../../../core/model/page/pageable-request.model';
 
 @Component({
   selector: 'app-subscription-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './subscription-form.component.html',
   styleUrl: './subscription-form.component.css'
 })
 export class SubscriptionFormComponent {
 
+  @Input() subscription?: SubscriptionResponse;
+  @Output() close = new EventEmitter<boolean>();
+
+  private fb = inject(NonNullableFormBuilder);
+  private subscriptionService = inject(SubscriptionService);
+
+  form = this.fb.group({
+    name: ['', Validators.required],
+    modalityName: ['', Validators.required],
+    price: [0, Validators.required],
+  });
+
+  private readonly modalityService = inject(ModalityService);
+
+  modalities: ModalityResponse[] = [];
+  page = new PageableRequest(0, 100, 'name');
+
+  ngOnInit() {
+
+    if (this.subscription) {
+      this.form.patchValue({
+        name: this.subscription.name,
+        modalityName: this.subscription.modality?.name,
+        price: this.subscription.price
+      });
+    }
+
+    this.loadModalities();
+
+  }
+
+  loadModalities(): void {
+    this.modalityService
+      .findAll(this.page)
+      .subscribe({
+        next: page => {
+          this.modalities = page.content;
+
+        },
+        error: err => console.error(err)
+
+      });
+  }
+
+  save(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const request: SubscriptionRequest = this.form.getRawValue();
+
+    if (this.subscription) {
+      this.subscriptionService
+        .update(this.subscription.id, request)
+        .subscribe(() => this.close.emit(true));
+
+    } else {
+      this.subscriptionService
+        .create(request)
+        .subscribe(() => this.close.emit(true));
+
+    }
+
+  }
+
+  ngOnChanges(): void {
+    if (!this.subscription) {
+      this.form.reset();
+      return;
+    }
+
+    this.form.patchValue({
+      name: this.subscription.name,
+    });
+  }
+
+  cancel() {
+    this.close.emit(false);
+  }
 }
--- frontend/src/app/features/subscription/subscription-list/subscription-list.component.ts
+++ frontend/src/app/features/subscription/subscription-list/subscription-list.component.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SubscriptionResponse } from '../../../core/model/subscription-response.model';
+import { SubscriptionService } from '../../../core/service/subscription.service';
+import { PageResponse } from '../../../core/model/page/page-response.model';
+import { PageableRequest } from '../../../core/model/page/pageable-request.model';
+import { FormBuilder } from '@angular/forms';
+import { SubscriptionFormComponent } from "../subscription-form/subscription-form.component";
 
 @Component({
   selector: 'app-subscription-list',
-  imports: [],
+  imports: [SubscriptionFormComponent],
   templateUrl: './subscription-list.component.html',
   styleUrl: './subscription-list.component.css'
 })
-export class SubscriptionListComponent {
+export class SubscriptionListComponent implements OnInit {
 
+  private readonly subscriptionService = inject(SubscriptionService);
+
+  subscriptions: SubscriptionResponse[] = [];
+
+  pageResponse?: PageResponse<SubscriptionResponse>;
+
+  page = new PageableRequest(0, 30, 'name');
+
+  selectedSubscription?: SubscriptionResponse;
+
+  totalElements = 0;
+
+  modalOpen = false;
+
+  private fb = inject(FormBuilder);
+  searchForm = this.fb.group({
+    name: [''],
+    modality: [''],
+    price: [''],
+    active: [''],
+  });
+
+  ngOnInit(): void {
+    this.loadSubscriptions();
+  }
+
+  loadSubscriptions(): void {
+    this.subscriptionService
+      .findAll(this.page)
+      .subscribe({
+
+        next: page => {
+          this.subscriptions = page.content;
+          this.totalElements = page.totalElements;
+        },
+
+        error: err => console.error(err)
+
+      });
+  }
+
+  search(): void {
+    this.page.page = 0;
+    this.loadSubscriptions();
+  }
+
+  clear(): void {
+    this.searchForm.reset();
+    this.page.page = 0;
+    this.loadSubscriptions();
+  }
+
+  openCreate(): void {
+    this.selectedSubscription = undefined;
+    this.modalOpen = true;
+  }
+
+  openEdit(subscription: SubscriptionResponse): void {
+    this.selectedSubscription = subscription;
+    this.modalOpen = true;
+  }
+
+  delete(id: number): void {
+    if (!confirm('Deseja realmente excluir este plano?')) {
+      return;
+    }
+    this.subscriptionService.delete(id).subscribe({
+      next: () => {
+        this.loadSubscriptions();
+      },
+      error: err => console.error(err)
+
+    });
+  }
+
+  closeModal(refresh: boolean): void {
+    this.modalOpen = false;
+    if (refresh) {
+      this.loadSubscriptions();
+    }
+  }
 }
+
