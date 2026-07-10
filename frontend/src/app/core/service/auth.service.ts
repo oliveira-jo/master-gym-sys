32cd1b6c77b155f57866f512b905fac8a97bbf3d
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-10 15:31:37 -0300

    feat(frontend): create auth service

--- frontend/src/app/core/service/auth.service.ts
+++ frontend/src/app/core/service/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { TokenResponse } from '../model/response/token-response';
+import { TokenService } from './token.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private readonly API = environment.apiUrl + '/oauth2/token';
+
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+    private router: Router) { }
+
+  login(username: string, password: string) {
+    const body = new HttpParams()
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password');
+
+    const headers = new HttpHeaders({
+      Authorization: 'Basic ' + btoa('myclientid:myclientsecret'),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+
+    return this.http.post<TokenResponse>(
+      this.API,
+      body.toString(),
+      { headers }
+    );
+  }
+
+  logout(): void {
+    this.tokenService.remove();
+    this.router.navigate(['/login']);
+  }
+
+
 }
