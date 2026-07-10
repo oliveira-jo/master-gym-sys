b8b0ee731ade86d8ddf152a52699b33f336dda5b
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-10 19:32:59 -0300

    refact(backend): user repository

--- backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/UserRepository.java
+++ backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/UserRepository.java
@@ -1,9 +1,13 @@
 package com.devjoliveira.mastergymsys.repositoty;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 
 import com.devjoliveira.mastergymsys.domain.User;
 import com.devjoliveira.mastergymsys.projection.UserDetailsProjection;
@@ -18,5 +22,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 				WHERE users.email = :email
 			""")
 	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+	@NonNull
+	Page<User> findAll(@NonNull Pageable pageable);
+
+	Optional<User> findByCpf(String cpf);
+
+	Optional<User> findByEmail(String cpf);
 
 }
