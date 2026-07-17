41e1402d254609b71c92742d64462fb812e153aa
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-17 12:09:17 -0300

    refact(backend): update payment reposiroty

--- backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/PaymentRepository.java
+++ backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/PaymentRepository.java
@@ -1,9 +1,22 @@
 package com.devjoliveira.mastergymsys.repositoty;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devjoliveira.mastergymsys.domain.Payment;
+import com.devjoliveira.mastergymsys.domain.enums.StatusPayment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+  List<Payment> findByStatus(StatusPayment status);
+
+  List<Payment> findByEnrollmentId(Long enrollmentId);
+
+  List<Payment> findByDueDate(LocalDate dueDate);
+
+  // Searches only for outstanding payments that are past due
+  List<Payment> findByDueDateBeforeAndStatus(LocalDate date, StatusPayment status);
 
 }
