26a65a037e8d482ee2a5921c02bc06f8249145c2
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-28 12:55:23 -0300

    refact(backend): add in enrollment repo find with search param

--- backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/EnrollmentRepository.java
+++ backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/EnrollmentRepository.java
@@ -1,9 +1,27 @@
 package com.devjoliveira.mastergymsys.repositoty;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devjoliveira.mastergymsys.domain.Enrollment;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
+  @Query("""
+        SELECT e
+        FROM Enrollment e
+        JOIN e.student s
+        WHERE
+        (:search IS NULL OR :search = '')
+        OR
+        LOWER(s.name) LIKE LOWER(CONCAT('%', :search, '%'))
+        OR
+        LOWER(s.email) LIKE LOWER(CONCAT('%', :search, '%'))
+        OR
+        CAST(e.id AS string) = :search
+      """)
+  Page<Enrollment> search(String search, Pageable pageable);
 
 }
