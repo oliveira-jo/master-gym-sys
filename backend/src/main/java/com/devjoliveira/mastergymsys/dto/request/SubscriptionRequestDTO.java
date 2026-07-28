4e9f9e9443ca7d9ea4d6e5e5e21e21146f566388
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-28 20:41:25 -0300

    refact(backend): refact dtos

--- backend/src/main/java/com/devjoliveira/mastergymsys/dto/request/SubscriptionRequestDTO.java
+++ backend/src/main/java/com/devjoliveira/mastergymsys/dto/request/SubscriptionRequestDTO.java
@@ -7,8 +7,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SubscriptionRequestDTO(
-    @NotBlank(message = "Name is required") String name,
-    @NotNull(message = "Price is required") @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero") BigDecimal price,
-    @NotNull(message = "ModalityId is required") Long modalityId) {
+
+        @NotBlank(message = "Name is required") String name,
+
+        @NotNull(message = "Price is required") @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero") BigDecimal price,
+
+        @NotNull(message = "ModalityId is required") Long modalityId) {
 
 }
