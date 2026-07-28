6e508a7b04f96ed71b7a37ad250ee163a634c84a
Jonathan Oliveira <devjoliveira@gmail.com>
2026-07-28 14:32:23 -0300

    fix(backend): fix payment repository with jpql in findHistory

--- backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/PaymentRepository.java
+++ backend/src/main/java/com/devjoliveira/mastergymsys/repositoty/PaymentRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 
 import com.devjoliveira.mastergymsys.domain.Payment;
@@ -13,14 +14,20 @@ import com.devjoliveira.mastergymsys.domain.enums.StatusPayment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-  Page<Payment> findByStatus(Pageable pageable, StatusPayment status);
-
-  Page<Payment> findByEnrollmentId(Pageable pageable, Long enrollmentId);
-
+  @Query("""
+          SELECT p
+          FROM Payment p
+          WHERE p.enrollment.id = :enrollmentId
+          ORDER BY p.dueDate DESC
+      """)
   List<Payment> findHistory(Long enrollmentId);
 
   @NonNull
   Page<Payment> findAll(Pageable pageable);
+
+  Page<Payment> findByStatus(Pageable pageable, StatusPayment status);
+
+  Page<Payment> findByEnrollmentId(Pageable pageable, Long enrollmentId);
 
   List<Payment> findByDueDate(LocalDate dueDate);
 
